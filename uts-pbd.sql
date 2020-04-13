8a611c32cc3246fa9654380be42d72563fbc84f9
Arya Naradana <52026986+naradanarya@users.noreply.github.com>
2020-04-13 23:10:41 +0700

    database

--- uts-pbd.sql
+++ uts-pbd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2020 at 04:38 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Waktu pembuatan: 13 Apr 2020 pada 16.47
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `uts-pbd`
+-- Database: `test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `datapoint`
+-- Struktur dari tabel `datapoint`
 --
 
 CREATE TABLE `datapoint` (
@@ -33,27 +33,26 @@ CREATE TABLE `datapoint` (
   `nama_spbu` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `latitude` varchar(11) DEFAULT NULL,
-  `longitude` varchar(11) DEFAULT NULL,
-  `warna` varchar(255) DEFAULT NULL
+  `longitude` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `datapoint`
+-- Dumping data untuk tabel `datapoint`
 --
 
-INSERT INTO `datapoint` (`id`, `nama_spbu`, `keterangan`, `latitude`, `longitude`, `warna`) VALUES
-(1, 'Monumen Nasional', NULL, '-6.1749344', '106.8217092', 'red'),
-(2, 'Istana Merdeka', NULL, '-6.1701238', '106.8219881', 'yellow'),
-(3, 'Badan Pajak', NULL, '-6.1757131', '106.8208509', 'white'),
-(4, 'Kementrian BUMN', NULL, '-6.1779104', '106.8213766', 'white'),
-(5, 'asd', 'asd', '-6.66460756', '106.9736981', NULL),
-(6, 'asdasda', 'asd1', '-6.17546764', '106.8271602', NULL),
-(7, 'asdasda', 'asdasd', '-6.17946759', '106.8941418', NULL);
+INSERT INTO `datapoint` (`id`, `nama_spbu`, `keterangan`, `latitude`, `longitude`) VALUES
+(1, 'Monumen Nasional', NULL, '-6.1749344', '106.8217092'),
+(2, 'Istana Merdeka', NULL, '-6.1701238', '106.8219881'),
+(3, 'Badan Pajak', NULL, '-6.1757131', '106.8208509'),
+(4, 'Kementrian BUMN', NULL, '-6.1779104', '106.8213766'),
+(5, 'asd', 'asd', '-6.66460756', '106.9736981'),
+(6, 'asdasda', 'asd1', '-6.17546764', '106.8271602'),
+(7, 'asdasda', 'asdasd', '-6.17946759', '106.8941418');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `konfigurasi`
+-- Struktur dari tabel `konfigurasi`
 --
 
 CREATE TABLE `konfigurasi` (
@@ -64,7 +63,7 @@ CREATE TABLE `konfigurasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `konfigurasi`
+-- Dumping data untuk tabel `konfigurasi`
 --
 
 INSERT INTO `konfigurasi` (`setting_id`, `namaweb`, `icon`, `logo`) VALUES
@@ -73,7 +72,7 @@ INSERT INTO `konfigurasi` (`setting_id`, `namaweb`, `icon`, `logo`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -85,7 +84,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `image`, `akses_level`) VALUES
@@ -98,41 +97,41 @@ INSERT INTO `users` (`id_user`, `username`, `password`, `image`, `akses_level`) 
 --
 
 --
--- Indexes for table `datapoint`
+-- Indeks untuk tabel `datapoint`
 --
 ALTER TABLE `datapoint`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `konfigurasi`
+-- Indeks untuk tabel `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   ADD PRIMARY KEY (`setting_id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `datapoint`
+-- AUTO_INCREMENT untuk tabel `datapoint`
 --
 ALTER TABLE `datapoint`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `konfigurasi`
+-- AUTO_INCREMENT untuk tabel `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   MODIFY `setting_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
